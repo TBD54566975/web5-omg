cfc3f678e8fdd7b80087fd9c9dd79f0b76b9a355
Mic Neale <michael.neale@gmail.com>
2023-03-23 20:15:02 +1100

    smartening the prompt

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const fs = require('fs');
 
 app.use(express.json())
 
+const appjs = fs.readFileSync('seed_app.html', 'utf8');
+
+
 app.get('/', (req, res) => {
   // load index.html as a string, and serve it
   const index = fs.readFileSync('index.html', 'utf8');
@@ -15,7 +18,6 @@ app.get('/', (req, res) => {
 
 app.get("/app", (req, res) => {
   // load seed.js as a string, and serve it. this is the starting point for the app that they will be editing.
-  const appjs = fs.readFileSync('seed_app.html', 'utf8');
   res.send(appjs);
 });
 
@@ -39,50 +41,69 @@ async function processOpenai(prompt, context_html) {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
   });
-  const appjs = fs.readFileSync('seed_app.html', 'utf8');
+
+  let chat_messages = [
+    {role: "system", "content": "You are a helpful assistant that modifies the html and web5 source code as directed."},              
+
+    {role: "user", content: "Some requirements: when a script tag of type 'module' is used, make sure functions are made accessible via window.functionName = functionName, or use the document object to attach onclick handlers to form elements."},
+    {role: "assistant", content: "ok understood."},
+
+    {role: "user", content: "write a minimal html page which shows pictures of lions"},
+    {role: "assistant", content : `<!DOCTYPE html>
+    <html>
+      <body>
+        <h1>Lions</h1>
+        <img src="lion1.jpg" alt="lion">
+        <img src="lion2.jpg" alt="lion">
+      </body>
+    </html>`},
+
+    {role: "user", content: "write an app that prints an alert with a random number when I press a button"},
+    {role: "assistant", content : `<!DOCTYPE html>
+    <html>
+      <head>
+        <title>Random Number Generator</title>
+      </head>
+      <body>
+        <h1>Random Number Generator</h1>
+        <button onclick="generateRandomNumber()">Generate Random Number</button>
+        <script type="module">
+          window.generateRandomNumber = function generateRandomNumber() {
+            var randomNumber = Math.floor(Math.random() * 100) + 1;
+            alert("Your random number is: " + randomNumber);
+          }
+        </script>
+      </body>
+    </html>`},
+
+
+    ];
+
+
+  if (context_html == appjs) {
+
+    // append to chat_messages: 
+    chat_messages = chat_messages.concat([
+      {role: "user", content: "Web5 is a new decentralized framework for building web apps. I will provide you with the web5 web app source code and you will modify it as directed."},
+      {role: "assistant", content: "ok. I will modify the code as directed and only return the code. What does web5 look like, can you provide me a minimal example?"},  
+      {role: "user", content: "The following is a minimal example of a web5 app that show the the style of the api and necessary boiler plate in the context of a single html page:\n" + context_html},              
+      {role: "assistant", content: "ok got it. I will base code on the provided example, and not add any unnecessary styles unless asked. I also note this is a single page app with no server side."},
+      {role: "user", content: "Build a web5 based on the example previously, as a single page app (no server to submit to) in the browser:" + prompt},          
+      ]);
+
+  } else {
+    // append to chat_messages: 
+    chat_messages = chat_messages.concat([
+      {role: "user", content: "Web5 is a new decentralized framework for building web apps. I will provide you with the web5 web app source code and you will modify it as directed, preserving styles and boilerplate."},
+      {role: "assistant", content: "ok. I will modify the code as directed and only return the code."},  
+      {role: "user", content: prompt + ":\n" + context_html},
+    ]);
+  }
 
   const openai = new OpenAIApi(configuration);  
   const completion = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
-    messages: [
-              {role: "system", "content": "You are a helpful assistant that modifies the html and web5 source code as directed."},              
-
-              {role: "user", content: "write a minimal html page which shows pictures of lions"},
-              {role: "assistant", content : `<!DOCTYPE html>
-              <html>
-                <body>
-                  <h1>Lions</h1>
-                  <img src="lion1.jpg" alt="lion">
-                  <img src="lion2.jpg" alt="lion">
-                </body>
-              </html>`},
-
-              {role: "user", content: "write an app that prints an alert with a random number when I press a button"},
-              {role: "assistant", content : `<!DOCTYPE html>
-              <html>
-                <head>
-                  <title>Random Number Generator</title>
-                </head>
-                <body>
-                  <h1>Random Number Generator</h1>
-                  <button onclick="generateRandomNumber()">Generate Random Number</button>
-                  <script>
-                    function generateRandomNumber() {
-                      var randomNumber = Math.floor(Math.random() * 100) + 1;
-                      alert("Your random number is: " + randomNumber);
-                    }
-                  </script>
-                </body>
-              </html>`},
-
-
-              {role: "user", content: "ok great. Web5 is a new decentralized framework for building web apps. I will provide you with the web5 web app source code and you will modify it as directed."},
-              {role: "assistant", content: "ok. I will modify the code as directed and only return the code. What does web5 look like, can you provide me a minimal example?"},
-              
-              {role: "user", content: "The following is a minimal example of a web5 app that show the the style of the api and necessary boiler plate in the contest of a single html page:\n" + context_html},
-              {role: "assistant", content: "ok got it. I will modify the code as directed, and not add any unnecessary styles unless asked. I also note this is a single page app with no server side."},
-              {role: "user", content: "Modify the the web5 app above as described in the following prompt, as a single page app (no server to submit to) in the browser:" + prompt},          
-              ],
+    messages: chat_messages,
   });
   console.log(completion.data.choices[0].message);
   
